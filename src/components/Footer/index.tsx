68a5c9b2c973ab9e452e59ed39a27b255c4eb5ec
Leon Montealegre <leonm99@gmail.com>
2022-05-28 20:16:00 -0400

    Edited footer slightly

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,5 @@
-
 import styles from "./index.module.scss";
 
-console.log(styles);
 
 export const Footer = () => (
     <footer className={styles["footer"]}>
@@ -11,15 +9,19 @@ export const Footer = () => (
             </span>
         </div>
         <div className={styles["footer-icons"]}>
-            <a href="https://www.instagram.com/leonmontealegre/"    className={styles["fa"] + " " + styles["fa-instagram"]} target="_blank" rel="noreferrer"></a>
-            <a href="https://twitter.com/KingDolphinGuy"            className={styles["fa"] + " " + styles["fa-twitter"]}   target="_blank" rel="noreferrer"></a>
-            <a href="https://www.linkedin.com/in/leonmontealegre/"  className={styles["fa"] + " " + styles["fa-linkedin"]}  target="_blank" rel="noreferrer"></a>
-            <a href="https://www.youtube.com/user/leonmontealegre/" className={styles["fa"] + " " + styles["fa-youtube"]}   target="_blank" rel="noreferrer"></a>
-            <a href="https://github.com/LeonMontealegre"            className={styles["fa"] + " " + styles["fa-github"]}    target="_blank" rel="noreferrer"></a>
+            <a href="https://www.instagram.com/leonmontealegre/"
+               className={styles["footer-icons-instagram"]} target="_blank" rel="noreferrer"></a>
+            <a href="https://twitter.com/KingDolphinGuy"
+               className={styles["footer-icons-twitter"]}   target="_blank" rel="noreferrer"></a>
+            <a href="https://www.linkedin.com/in/leonmontealegre/"
+               className={styles["footer-icons-linkedin"]}  target="_blank" rel="noreferrer"></a>
+            <a href="https://www.youtube.com/user/leonmontealegre/"
+               className={styles["footer-icons-youtube"]}   target="_blank" rel="noreferrer"></a>
+            <a href="https://github.com/LeonMontealegre"
+               className={styles["footer-icons-github"]}    target="_blank" rel="noreferrer"></a>
         </div>
         <div className={styles["footer-right"]}>
             <span>leonmontealegre.com</span>
         </div>
     </footer>
 );
-
