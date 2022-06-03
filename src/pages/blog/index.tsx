fdfdcbc9de5b59a34291e744527ab05e8f2c5068
Leon Montealegre <leonm99@gmail.com>
2022-06-03 17:43:06 -0400

    Added initial mdx config

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,17 @@
+import type {NextPage} from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./index.module.scss";
+
+
+const Blog: NextPage = () => {
+    return (
+        <div className="">
+        </div>
+    );
+}
+
+
+export default Blog;
