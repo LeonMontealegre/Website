900850b8ba528d37d22948fc816bfac0aed562e8
Leon Montealegre <leonm99@gmail.com>
2022-06-03 16:43:08 -0400

    Added initial menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import type {NextPage} from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 
 
 const Home: NextPage = () => {
     return (
-        <div>
-            Test
+        <div className={styles["container"]}>
+            <Link href="/"          ><div className={styles["circle"]}><p>Home      </p></div></Link>
+            <Link href="/about"     ><div className={styles["circle"]}><p>About     </p></div></Link>
+            <Link href="/blog"      ><div className={styles["circle"]}><p>Blog      </p></div></Link>
+            <Link href="/projects"  ><div className={styles["circle"]}><p>Projects  </p></div></Link>
+            <Link href="/animations"><div className={styles["circle"]}><p>Animations</p></div></Link>
+            <Link href="/models"    ><div className={styles["circle"]}><p>Models    </p></div></Link>
         </div>
-    )
+    );
 }
 
 
